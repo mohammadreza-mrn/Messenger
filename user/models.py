5897f13c84055e9bb9edc62d34b57fab5c8cf35a
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2025-05-14 23:33:30 +0330

    change user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,PermissionsMixin,BaseUserManager
 
-# Create your models here.
+
+
+class CustomUser(AbstractUser,PermissionsMixin):
+
+    profile_picture = models.ImageField(upload_to="profile_pictures",verbose_name="Profile Picture",null=True,blank=True)
+    last_seen = models.DateTimeField(auto_now=True,verbose_name="Last Seen",null=True,blank=True)
