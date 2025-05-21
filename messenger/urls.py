f940f9caf4ac0d4fa6d614f4ad740559adab2016
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2025-05-21 16:26:08 +0330

    define home view url

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,10 +4,15 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("user.urls", namespace="user")),
+
+    # Home View
+    path("",HomeView.as_view(),name = "homeview"),
 ]
 
 if settings.DEBUG:
